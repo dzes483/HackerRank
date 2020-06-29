b07ce69f73d5ea1f54e86ef060569a75ba667d04
Jessica <jban@sdf.org>
2020-06-29 17:45:35 +0300

    Add new file "find_a_string.py"

---
+++ 3. Strings/find_a_string.py
@@ -0,0 +1,26 @@
+def count_substring(string, sub_string):
+    count = 0
+    start = 0
+    end = len(sub_string)
+    for i in range(len(string)):
+        result = string.find(sub_string, start, end)
+        if result != -1:
+            count += 1
+        start += 1
+        end += 1
+    return count
+
+
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+
+    count = count_substring(string, sub_string)
+    print(count)
+
+
+string = 'ABCDCDC'
+
+chunk = string[0:3]
+print(chunk)
