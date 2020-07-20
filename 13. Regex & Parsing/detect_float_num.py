5d28ed75b4da1df82f54c4ff001bbf3e4b49fd67
Jessica <jban@sdf.org>
2020-07-20 20:53:10 +0300

    Fix variable name

--- 13. Regex & Parsing/detect_float_num.py
+++ 13. Regex & Parsing/detect_float_num.py
@@ -3,8 +3,8 @@ import re
 num_cases = int(input())
 
 for i in range(num_cases):
-    match = re.fullmatch(r'^[+-]?[\d]*\.+[\d]+$', input())
-    if match:
+    m = re.fullmatch(r'^[+-]?[\d]*\.+[\d]+$', input())
+    if m:
         print(True)
     else:
         print(False)
