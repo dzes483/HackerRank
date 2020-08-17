2b724260da4bb513116b0fe42dc6821c03537093
Jessica <jban@sdf.org>
2020-08-17 08:12:18 +0300

    Add new file "inner_and_outer.py"

---
+++ P. Numpy/inner_and_outer.py
@@ -0,0 +1,7 @@
+import numpy
+
+arr_a = numpy.array([int(i) for i in input().split()])
+arr_b = numpy.array([int(i) for i in input().split()])
+
+print(numpy.inner(arr_a, arr_b))
+print(numpy.outer(arr_a, arr_b))
