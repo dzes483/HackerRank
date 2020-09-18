dd23c8b3e33985cb43a1034db09d72c5cab67d11
Jessica <jban@sdf.org>
2020-09-18 18:24:39 +0300

    Add new file "sock_merchant.py"

---
+++ Problem-Solving/B-Implementation/sock_merchant.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+from collections import Counter
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    return sum([(v//2) for k, v in Counter(ar).items()])
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
