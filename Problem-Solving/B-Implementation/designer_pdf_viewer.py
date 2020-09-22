c0d40e3e7cc526020936a152ab97277225d39599
Jessica <jban@sdf.org>
2020-09-22 18:45:16 +0300

    Add new file "designer_pdf_viewer.py"

---
+++ Problem-Solving/B-Implementation/designer_pdf_viewer.py
@@ -0,0 +1,29 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+import string
+
+# Complete the designerPdfViewer function below.
+def designerPdfViewer(h, word):
+    d = {list(string.ascii_lowercase)[i]: h[i] for i in range(len(h))}
+    letters = []
+    for letter in word:
+        letters.append(d[letter])
+    return max(letters) * len(word)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    h = list(map(int, input().rstrip().split()))
+
+    word = input()
+
+    result = designerPdfViewer(h, word)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
